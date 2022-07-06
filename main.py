31abdb074178a6131d7588c3b0db222c7de64835
Ilya <73064012+pijawca@users.noreply.github.com>
2022-07-06 23:54:45 +0700

    micro fix

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from google_currency import convert
 
 api_id = #
 api_hash = "#"
-
 async def main():
-    async with Client("free", api_id=api_id, api_hash=api_hash) as app:
-        money = get('#')
+    async with Client("pijawca", api_id=api_id, api_hash=api_hash) as app:
+        money = get('https://raw.githubusercontent.com/pijawca/changenn/master/money.txt').text
         json_object = loads(convert('rub', 'usd', int(money)))
         await app.update_profile(last_name='[' + json_object['amount'] + ']')
 
